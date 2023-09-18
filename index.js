66be017277eeecaa2c206cd583c65c3036fed7ea
Lasma Poksane <laasmapoksane@gmail.com>
2023-09-18 20:32:48 +0200

    add an endpoint to add goal to the database

--- index.js
+++ index.js
@@ -13,6 +13,23 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT;
 
+app.post("/add-a-goal", async (req, res) => {
+  try {
+    const { goalName, category } = req.body;
+
+    const result = await db("goals").insert({
+      goal_name: goalName,
+      category: category,
+    });
+
+    return res.status(201).json({
+      message: "Goal added successfully.",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error." });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is listening for requests on port ${PORT}`);
 });
