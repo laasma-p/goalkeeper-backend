100bdd63f2c2a9e2f31d921eea95db6e9600f608
Lasma Poksane <laasmapoksane@gmail.com>
2024-03-31 19:46:03 +0200

    add models synchronization

--- sequelize.js
+++ sequelize.js
@@ -15,6 +15,8 @@ const testConnection = async () => {
   try {
     await sequelize.authenticate();
     console.log("Connection has been established successfully.");
+    await sequelize.sync();
+    console.log("All models were synchronized successfully.");
   } catch (error) {
     console.error("Unable to connect to the database:", error);
   }
